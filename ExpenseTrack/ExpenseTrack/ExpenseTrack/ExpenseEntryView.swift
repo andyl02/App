a0f8249c0c7824f7e88dd0485080c354b56f106b
andyl02 <80433664+andyl02@users.noreply.github.com>
2023-10-14 20:03:00 +1100

    Update code

--- ExpenseTrack/ExpenseTrack/ExpenseTrack/ExpenseEntryView.swift
+++ ExpenseTrack/ExpenseTrack/ExpenseTrack/ExpenseEntryView.swift
@@ -1,31 +1,40 @@
 // ExpenseEntryView.swift
+
 import SwiftUI
 
 struct ExpenseEntryView: View {
     @EnvironmentObject var expenseManager: ExpenseManager
     @State private var category: String = ""
     @State private var amount: Double = 0.0
-    @State private var date: Date = Date()
+    @State private var feedback: String = ""  // Added feedback state
     
     var body: some View {
         Form {
             Section(header: Text("Expense Details")) {
                 TextField("Category", text: $category)
-                TextField("Amount", value: $amount, format: .currency(code: expenseManager.currencyCode))
-                DatePicker("Date", selection: $date, displayedComponents: .date)
+                TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? ""))
             }
             
             Button(action: addExpense) {
-                Text("Add Expense")
+                Text("Save Expense")
+            }
+            
+            if !feedback.isEmpty {
+                Text(feedback)
+                    .foregroundColor(.red)
             }
         }
-        .navigationTitle("Expense Entry")
+        .navigationTitle("Add Expense")
     }
     
     private func addExpense() {
-        expenseManager.addExpense(Expense(category: category, amount: amount, date: date))
-        category = ""
-        amount = 0.0
-        date = Date()
+        if category.isEmpty || amount <= 0 {
+            feedback = "Please enter valid details."
+            return
+        }
+        
+        let expense = Expense(category: category, amount: amount, date: Date())
+        expenseManager.addExpense(expense)
+        feedback = "Expense added successfully!"
     }
 }
