26506c4e43a72ad1750635edbc45b92730caaa49
andyl02 <80433664+andyl02@users.noreply.github.com>
2023-10-14 19:50:52 +1100

    implement a way to set budgets for caategories

--- ExpenseTrack/ExpenseTrack/ExpenseTrack/BudgetTrackingView.swift
+++ ExpenseTrack/ExpenseTrack/ExpenseTrack/BudgetTrackingView.swift
@@ -4,43 +4,40 @@ import SwiftUI
 
 struct BudgetTrackingView: View {
     @EnvironmentObject var expenseManager: ExpenseManager
-    @State private var budgets: [String: Double] = [:]
+    @State private var selectedCategory: String = ""
+    @State private var budgetAmount: Double = 0.0
     
     var body: some View {
         VStack {
             List {
-                ForEach(budgets.sorted(by: { $0.key < $1.key }), id: \.key) { category, amount in
+                ForEach(expenseManager.categories, id: \.self) { category in
                     HStack {
                         Text(category)
-                            .font(.headline)
                         Spacer()
-                        Text("\(amount, specifier: "%.2f")")
-                            .font(.headline)
+                        Text("\(expenseManager.getBudget(for: category) ?? 0.0, specifier: "%.2f")")
                     }
                 }
             }
-            .listStyle(InsetGroupedListStyle())
             
-            Button(action: calculateBudgets) {
-                Text("Calculate Budgets")
+            Picker("Category", selection: $selectedCategory) {
+                ForEach(expenseManager.categories, id: \.self) { category in
+                    Text(category).tag(category)
+                }
+            }
+            .padding()
+            
+            TextField("Budget Amount", value: $budgetAmount, format: .currency(code: Locale.current.currency?.identifier ?? ""))
+            .padding()
+            
+            Button(action: setBudget) {
+                Text("Set Budget")
             }
             .padding()
         }
         .navigationTitle("Budget Tracking")
-        .onAppear {
-            calculateBudgets()
-        }
     }
     
-    private func calculateBudgets() {
-        budgets = [:]
-        
-        for expense in expenseManager.expenses {
-            if let currentAmount = budgets[expense.category] {
-                budgets[expense.category] = currentAmount + expense.amount
-            } else {
-                budgets[expense.category] = expense.amount
-            }
-        }
+    private func setBudget() {
+        expenseManager.setBudget(for: selectedCategory, amount: budgetAmount)
     }
 }
