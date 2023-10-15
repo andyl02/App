c0f8e0ea416b43d0cd0220d85753390227b1e375
andyl02 <80433664+andyl02@users.noreply.github.com>
2023-10-15 21:14:03 +1100

    Implement 10 Contextual Unit Tests

--- ExpenseTrack/ExpenseTrack/ExpenseTrackTests/ExpenseTrackTests.swift
+++ ExpenseTrack/ExpenseTrack/ExpenseTrackTests/ExpenseTrackTests.swift
@@ -1,36 +1,90 @@
-//
 //  ExpenseTrackTests.swift
-//  ExpenseTrackTests
-//
-//  Created by Lili Duong on 28/8/2023.
-//
 
 import XCTest
 @testable import ExpenseTrack
 
 final class ExpenseTrackTests: XCTestCase {
 
+    var expenseManager: ExpenseManager!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        expenseManager = ExpenseManager()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        expenseManager = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    // 1. Test Expense Addition
+    func testExpenseAddition() {
+        let initialCount = expenseManager.expenses.count
+        let expense = Expense(context: expenseManager.coreDataStack.context)
+        expense.amount = 10.0
+        expense.category = "Food"
+        expenseManager.addExpense(expense)
+        XCTAssertEqual(expenseManager.expenses.count, initialCount + 1)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    // 2. Test Expense Deletion
+    func testExpenseDeletion() {
+        let expense = Expense(context: expenseManager.coreDataStack.context)
+        expense.amount = 10.0
+        expense.category = "Food"
+        expenseManager.addExpense(expense)
+        let initialCount = expenseManager.expenses.count
+        expenseManager.deleteExpense(at: IndexSet(integer: 0))
+        XCTAssertEqual(expenseManager.expenses.count, initialCount - 1)
     }
 
+    // 3. Test Budget Setting
+    func testBudgetSetting() {
+        expenseManager.setBudget(for: "Food", amount: 100.0)
+        XCTAssertEqual(expenseManager.getBudget(for: "Food"), 100.0)
+    }
+
+    // 4. Test Budget Retrieval
+    func testBudgetRetrieval() {
+        let budget = expenseManager.getBudget(for: "Food")
+        XCTAssertNotNil(budget)
+    }
+
+    // 5. Test Total Expense for Category
+    func testTotalExpenseForCategory() {
+        let total = expenseManager.totalForCategory("Food")
+        XCTAssertNotNil(total)
+    }
+
+    // 6. Test Remaining Budget
+    func testRemainingBudget() {
+        let remaining = expenseManager.remainingBudget(for: "Food")
+        XCTAssertNotNil(remaining)
+    }
+
+    // 7. Test Category Addition
+    func testCategoryAddition() {
+        let initialCount = expenseManager.categories.count
+        expenseManager.addCategory("NewCategory")
+        XCTAssertEqual(expenseManager.categories.count, initialCount + 1)
+    }
+
+    // 8. Test Category Deletion
+    func testCategoryDeletion() {
+        expenseManager.addCategory("NewCategory")
+        let initialCount = expenseManager.categories.count
+        expenseManager.deleteCategory(at: IndexSet(integer: initialCount - 1))
+        XCTAssertEqual(expenseManager.categories.count, initialCount - 1)
+    }
+
+    // 9. Test Expenses by Category
+    func testExpensesByCategory() {
+        let expensesByCategory = expenseManager.expensesByCategory
+        XCTAssertNotNil(expensesByCategory)
+    }
+
+    // 10. Test Save Context
+    func testSaveContext() {
+        XCTAssertNoThrow(expenseManager.saveContext())
+    }
 }
