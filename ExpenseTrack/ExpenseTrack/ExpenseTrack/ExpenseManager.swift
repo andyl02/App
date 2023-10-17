b7b38141011c6a8b73ef08e8736057a936c8156c
andyl02 <80433664+andyl02@users.noreply.github.com>
2023-10-17 20:32:44 +1100

    Implement Error Handling

--- ExpenseTrack/ExpenseTrack/ExpenseTrack/ExpenseManager.swift
+++ ExpenseTrack/ExpenseTrack/ExpenseTrack/ExpenseManager.swift
@@ -4,76 +4,49 @@ import CoreData
 
 /// `ExpenseManager` is a class that manages all expense-related operations.
 ///
-/// This class provides published properties for expenses, categories, budgets, and expenses by category. It also provides methods for fetching expenses and budgets from Core Data, adding and deleting expenses and categories, setting and getting budgets for categories, calculating total expenses for a category, calculating the remaining budget for a category, saving the Core Data context, and fetching data from an API.
+/// This class provides published properties for expenses, categories, budgets, and expenses by category. It also provides methods for fetching expenses from Core Data, adding and deleting expenses and categories, setting and getting budgets for categories, calculating total expenses for a category, calculating the remaining budget for a category, saving the Core Data context, and fetching data from an API.
 struct APIExpense: Decodable {
     let amount: Double
     let category: String
 }
+
 /// Manages all expense-related operations.
 class ExpenseManager: ObservableObject {
+    
     /// An array of expenses.
-        @Published var expenses: [Expense] = []
-        
-        /// An array of categories.
-        @Published var categories: [String] = ["Food", "Transport", "Entertainment", "Utilities", "Rent", "Miscellaneous"]
-        
-        /// A dictionary of budgets keyed by category.
-        @Published var budgets: [String: Double] = [:]
-        
-        /// A dictionary of expenses by category.
-        @Published var expensesByCategory: [String: Double] = [:]
-        
-        /// The Core Data stack.
-        var coreDataStack = CoreDataStack.shared
+    @Published var expenses: [Expense] = []
+    
+    /// An array of categories.
+    @Published var categories: [String] = ["Food", "Transport", "Entertainment", "Utilities", "Rent", "Miscellaneous"]
+    
+    /// A dictionary of budgets keyed by category.
+    @Published var budgets: [String: Double] = [:]
+    
+    /// A dictionary of expenses by category.
+    @Published var expensesByCategory: [String: Double] = [:]
+    
+    /// The Core Data stack.
+    var coreDataStack = CoreDataStack.shared
     
     /// Initializes the expense manager.
     init() {
-        fetchExpenses()
-        fetchBudgets()
-        fetchDataFromAPI()
-    }
-
-    /// Fetches expenses from Core Data.
-    func fetchExpenses() {
-        let fetchRequest: NSFetchRequest<Expense> = Expense.fetchRequest()
         do {
-            expenses = try coreDataStack.context.fetch(fetchRequest)
-        } catch let error as NSError {
-            print("Could not fetch. \(error), \(error.userInfo)")
+            try fetchExpenses()
+            fetchDataFromAPI()
+        } catch let error {
+            print("Initialization failed: \(error)")
         }
     }
     
-    /// Fetches budgets from Core Data.
-    func fetchBudgets() {
-        let fetchRequest: NSFetchRequest<Budget> = Budget.fetchRequest()
+    /// Fetches expenses from Core Data.
+    /// - Throws: An error if the fetch operation fails.
+    func fetchExpenses() throws {
+        let fetchRequest: NSFetchRequest<Expense> = Expense.fetchRequest()
         do {
-            let fetchedBudgets = try coreDataStack.context.fetch(fetchRequest)
-            for budget in fetchedBudgets {
-                if let category = budget.category {
-                    budgets[category] = budget.amount
-                }
-            }
-        } catch let error as NSError {
-            print("Could not fetch budgets. \(error), \(error.userInfo)")
+            expenses = try coreDataStack.context.fetch(fetchRequest)
+        } catch {
+            throw ExpenseManagerError.fetchFailed
         }
-    }
-
-    /// Adds a new expense.
-    /// - Parameter expense: The expense to add.
-    func addExpense(_ expense: Expense) {
-        expenses.append(expense)
-        saveContext()
-    }
-
-    /// Deletes an expense.
-    /// - Parameter offsets: The index set of the expense to delete.
-    func deleteExpense(at offsets: IndexSet) {
-        for index in offsets {
-            let expense = expenses[index]
-            coreDataStack.context.delete(expense)
-        }
-        saveContext()
-        fetchExpenses()
     }
 
     /// Adds a new category.
@@ -135,6 +108,14 @@ class ExpenseManager: ObservableObject {
         }
     }
 
+    /// Defines errors that can occur in `ExpenseManager`.
+    enum ExpenseManagerError: Error {
+        case fetchFailed
+        case saveFailed
+        case deleteFailed
+    }
+    
+    
     /// Fetches data from an API.
     func fetchDataFromAPI() {
         NetworkManager.shared.fetchJSONData(url: "https://api.example.com/data") { (decodedData: [APIExpense]?, error) in
